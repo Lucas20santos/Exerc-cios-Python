a9e1b2249e3b17c6aa337fa2cbbd3f100936c300
lucas de souza santos <lucass.santoss210@gmail.com>
2022-06-28 23:49:54 -0300

    exercicio 001

--- EstruturaDecisao/exe001.py
+++ EstruturaDecisao/exe001.py
@@ -1,1 +1,14 @@
-# 
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Operador Ternário
+Maior = num1 if num1 > num2 else num2
+
+print(f"O maior é: {Maior}")
+
+if num1 > num2:
+    print(f"O maior é: {num1}")
+else:
+    print(f"O maior é: {num2}")
