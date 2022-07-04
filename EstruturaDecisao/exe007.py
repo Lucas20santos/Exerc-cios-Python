606c2ca1dc89242057bcc06ed9577582ceb5be05
lucas de souza santos <lucass.santoss210@gmail.com>
2022-07-04 19:15:23 -0300

    subindo exercicios

---
+++ EstruturaDecisao/exe007.py
@@ -0,0 +1,4 @@
+# Faça um Programa que leia três números e mostre o maior e o menor deles.
+# será seguindo o mesmo 
+
+
---
+++ EstruturaDecisao/isIterger.py
@@ -0,0 +1,7 @@
+def isInteiro(*num):
+    pass
+
+def DemoException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
